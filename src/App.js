7ced42f54f0feefd93170865e52fe44fa74def27
Justin ODea <odeajustin@gmail.com>
2020-09-17 15:13:32 -0600

    initial commit for React course

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import TodoList from './todos/TodoList';
+import './App.css';
+
+
+const App = () => (
+
+    <div className="App">
+        <TodoListItem/>
+    </div>
+    
+);
+
+export default hot(module) (App);
